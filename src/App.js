35028b8d8c2420527d61e6d5958904170708d9be
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2024-01-04 15:14:49 +0530

    login

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import './App.css';
 import { Typography, Button, IconButton, Stack, TextField, InputAdornment, Radio, RadioGroup, FormControl, FormLabel, FormControlLabel } from '@mui/material';
 import { red } from '@mui/material/colors';
 
-
-
-
-
 function App() {
   return (
     <div className="App">
-    <div>
-    <h2 className='title-text'>etutor</h2>
-    <Button className="btn btn-danger" variant='contained'>Register</Button>
-    </div>
-     <div className='container'>
-
-     </div>
+  
     </div>
   );
 }
